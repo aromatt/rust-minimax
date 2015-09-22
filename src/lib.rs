a2c8372b6043e4e0e3bfc8ff114373f2fb4a678d
Helge Skogly Holm <helge.holm@gmail.com>
2015-09-22 15:29:50 +0200

    An attempt at working correctly.

--- src/lib.rs
+++ src/lib.rs
@@ -9,23 +9,41 @@ pub struct Minimax;
 impl Minimax {
     pub fn best_move
         <State: Copy, Move: Copy, Value: PartialOrd, GameType: Game<State, Move, Value>>
-        (depth: i32, game: GameType, root: State) -> Move
+        (depth: i32, game: &GameType, root: State) -> Move
     {
+        let (mv, _score) = Minimax::min_max(depth, game, root, false);
+        mv.expect("no moves")
+    }
+
+    fn min_max
+        <State: Copy, Move: Copy, Value: PartialOrd, GameType: Game<State, Move, Value>>
+        (depth: i32, game: &GameType, root: State, min: bool) -> (Option<Move>, Value)
+    {
+        if depth == 0 {
+            return (None, game.eval(root));
+        }
+
         let moves = game.get_moves(root);
+        if moves.len() == 0 {
+            return (None, game.eval(root));
+        }
         
         let (first, rest) = moves.split_at(1);
-        let mut muu : Move = first[0];
-        let mut bezt = game.eval(game.apply(root, muu));
+
+        let mut muu: Move = first[0];
+        let (_subMove, mut bezt) = Minimax::min_max(depth - 1, game, game.apply(root, muu), !min);
         for &m in rest {
             let new_state = game.apply(root, m);
-            let vallie = game.eval(new_state);
-            if vallie > bezt {
+            let (_subMove, vallie) = Minimax::min_max(depth - 1, game, new_state, !min);
+            if (!min && vallie > bezt) || 
+                (min && vallie < bezt)
+            {
                 bezt = vallie;
                 muu = m;
             }
         }
 
-        muu
+        (Some(muu), bezt)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
 extern crate minimax;
 
 use minimax::{Game, Minimax};
+use std::vec::Vec;
 
 struct TestGame;
 
 impl Game<i32, i32, f32> for TestGame {
     fn get_moves(&self, root: i32) -> Vec<i32> {
-        let res = vec![root + 1, root + 0];
-        res
+        let mut moves = Vec::new();
+        if root < 100 { moves.push(1); }
+        if root < 99 { moves.push(2); }  
+        if root < 98 { moves.push(3); }
+        if root < 96 { moves.push(5); }
+        moves
     }
     
     fn eval(&self, state: i32) -> f32 {
-        return state as f32;
+        if state == 100 {
+            -1.0
+        } else {
+            0.0
+        }
     }
 
     fn apply(&self, state: i32, m: i32) -> i32 {
-        return (state + m) % 10;
+        state + m
     }
 }
 
 fn main() {
     let g = TestGame;
-    let muv = Minimax::best_move(1, g, 1);
+    let muv = Minimax::best_move(100, &g, 95);
     println!("helo thar: {}", muv);
 }
